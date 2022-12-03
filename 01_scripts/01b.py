c68674fa03b0067b799e50ed0f39eb0b8a96208a
Eric Normandeau <eric.normandeau.qc@gmail.com>
2022-12-03 16:56:10 -0500

    01b solved

---
+++ 01_scripts/01b.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Solution to Advent of Code problem (see file name)
+
+Usage:
+    <program> input_file
+"""
+
+# Modules
+import sys
+
+# Parse user input
+try:
+    input_file = sys.argv[1]
+except:
+    print(__doc__)
+    sys.exit(1)
+
+# Solve problem
+elves = [sum([int(y) for y in x.strip().split("\n")])
+        for x in open(input_file).read().strip().split("\n\n")]
+
+print(sum(sorted(elves)[-3:]))

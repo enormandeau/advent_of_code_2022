5bbd105bb64c2a7a4e04688d1770f01678463d76
Eric Normandeau <eric.normandeau.qc@gmail.com>
2022-12-03 16:42:17 -0500

    First commit

---
+++ 01a.py
@@ -0,0 +1,19 @@
+#!/usr/env/bin python3
+"""Solution to Advent of Code problem (see file name)
+
+Usage:
+    <program> input_file
+"""
+
+# Modules
+import sys
+
+# Parse user input
+try:
+    input_file = sys.argv[1]
+except:
+    print(__doc__)
+    sys.exit(1)
+
+# Solve problem
+
